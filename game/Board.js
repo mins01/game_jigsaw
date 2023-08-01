fec1ae60f443b0cd702969657bed2c2f53b7e7b2
mins01 <mins01.lycos.co.kr@gmail.com>
2023-08-01 14:11:22 +0900

    섞기, 고정된 피스

--- game/Board.js
+++ game/Board.js
@@ -5,6 +5,7 @@ import Piece from "./Piece.js";
 class Board{
     board = null;
     pieces = [];
+    usedPieces = [];
     constructor(board){
         this.board = board;
     }
@@ -14,21 +15,57 @@ class Board{
     get divY(){ return parseInt(this.board.style.getPropertyValue('--div-y')); }
     set divY(v){ this.board.style.setProperty('--div-y',v); }
 
-    get width(){ return this.board.style.getPropertyValue('--jigsaw-width'); }
-    set width(v){ this.board.style.setProperty('--jigsaw-width',v); }
-    get height(){ return this.board.style.getPropertyValue('--jigsaw-height'); }
-    set height(v){ this.board.style.setProperty('--jigsaw-height',v); }
+    get width(){ return parseInt(this.board.style.getPropertyValue('--jigsaw-width')); }
+    set width(v){ this.board.style.setProperty('--jigsaw-width',v+'px'); }
+    get height(){ return parseInt(this.board.style.getPropertyValue('--jigsaw-height')); }
+    set height(v){ this.board.style.setProperty('--jigsaw-height',v+'px'); }
 
-    
+    initEvent(){
+        this.board.addEventListener('pointerdown',this.pointerdown)
+        this.board.addEventListener('pointerup',this.pointerup)
+        
+    }
+    syncZIndex(){
+        this.usedPieces.sort((a,b)=>{
+            return a.zIndex - b.zIndex;
+        })
+        this.usedPieces.forEach((piece,idx) => {
+            piece.zIndex = idx+1+10;
+        });
+    }
+    pointerdown = (event)=>{
+        let target = ElementMove.findTarget(event.target);
+        if(!target) return;
+        let piece = target.piece??null;
+        piece.zIndex = 999999;
+    }
+    pointerup = (event)=>{
+        let target = ElementMove.findTarget(event.target);
+        if(!target) return;
+        let piece = target.piece??null;
+        let rect = target.getBoundingClientRect();
+        let pos = ElementMove.pos(target);
+
+        if(Math.abs(pos.x/rect.width) < 0.3 && Math.abs(pos.y/rect.height) < 0.3){
+            ElementMove.moveTo(target,0,0);
+            piece.fixed = true;
+        }
+        this.syncZIndex()
+    }
+
+
     draw(){
         this.board.innerHTML = null;
         for(let i=this.pieces.length,m=this.divX * this.divY;i<m;i++){ //필요 피스 수 만큼 늘림 (늘어난 걸 줄이지는 않음)
             this.pieces.push(new Piece());
         }
         let i = 0;
-        for(let iY=0,mY=this.divX;iY<mY;iY++){
+        this.usedPieces = [];
+        for(let iY=0,mY=this.divY;iY<mY;iY++){
             for(let iX=0,mX=this.divX;iX<mX;iX++){
                 let piece = this.pieces[i++];
+                piece.fixed = false;
+                this.usedPieces.push(piece);
                 piece.posX = iX;
                 piece.posY = iY;
                 piece.shapeTop = Piece.randPN();
@@ -42,10 +79,21 @@ class Board{
                 else{ piece.shapeTop = this.pieces[piece.posX + (piece.posY -1) * mX].shapeBottom=='n'?'p':'n' } //위 피스의 도출에 싱크
                 if(piece.posX ===mX-1 ){ piece.shapeRight = '0';}
                 if(piece.posY ===mY-1 ){ piece.shapeBottom = '0';}
-                this.board.appendChild(piece.piece)
+                this.board.appendChild(piece.wrap)
             }
         }
+        this.syncZIndex();
+    }
 
+    shuffle(){
+        this.usedPieces.forEach((piece,idx)=>{
+            ElementMove.moveTo(piece.wrap,
+                (Math.random()-0.5) * 4 * this.width  ,
+                (Math.random()-0.5) * 4 * this.height
+                )
+            ElementMove.isolate('in',piece.wrap)
+        })
+        
     }
 
 }
--- game/Piece.js
+++ game/Piece.js
@@ -1,30 +1,31 @@
 "use strict";
 
 class Piece{
-    piece=null;
+    wrap=null;
     shape=null;
     constructor(){
         this.#create();
     }
     #create(){
         // <div class="piece-wrap" data-pos-x="0" data-pos-y="0"><div class="piece-shape" data-shape-top="p" data-shape-right="p" data-shape-bottom="p" data-shape-left="p"></div></div>
-        this.piece = document.createElement('div');
-        this.piece.className = 'piece-wrap';
-        this.piece.dataset.posX = '0';
-        this.piece.dataset.posY = '0';
+        this.wrap = document.createElement('div');
+        this.wrap.className = 'piece-wrap element-move-target';
+        this.wrap.dataset.posX = '0';
+        this.wrap.dataset.posY = '0';
         this.shape = document.createElement('div');
         this.shape.className = 'piece-shape';
         this.shape.dataset.shapeTop= '0';
         this.shape.dataset.shapeRight= '0';
         this.shape.dataset.shapeBottom= '0';
         this.shape.dataset.shapeLeft= '0';
-        this.piece.appendChild(this.shape);
+        this.wrap.appendChild(this.shape);
+        this.wrap.piece = this
     }
 
-    get posX(){ return parseInt(this.piece.dataset.posX); }
-    set posX(v){ this.piece.dataset.posX = v; }
-    get posY(){ return parseInt(this.piece.dataset.posY); }
-    set posY(v){ this.piece.dataset.posY = v; }
+    get posX(){ return parseInt(this.wrap.dataset.posX); }
+    set posX(v){ this.wrap.dataset.posX = v; }
+    get posY(){ return parseInt(this.wrap.dataset.posY); }
+    set posY(v){ this.wrap.dataset.posY = v; }
 
     get shapeTop(){ return this.shape.dataset.shapeTop; }
     set shapeTop(v){ this.shape.dataset.shapeTop = v; }
@@ -35,11 +36,14 @@ class Piece{
     get shapeLeft(){ return this.shape.dataset.shapeLeft; }
     set shapeLeft(v){ this.shape.dataset.shapeLeft = v; }
 
+    get fixed(){ return this.wrap.classList.contain('fixed');}
+    set fixed(v){ v?this.wrap.classList.add('fixed'):this.wrap.classList.remove('fixed');}
+
+    get zIndex(){ return parseInt(this.wrap.style.zIndex??0); }
+    set zIndex(v){ this.wrap.style.zIndex = v;}
+
     static randPN(){
-        if(Math.floor(Math.random()*2)===0){
-            return 'n'
-        };
-        return 'p'
+        return Math.floor(Math.random()*2)===0?'n':'p'
     }
 }
 
